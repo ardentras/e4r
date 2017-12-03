02d2055cdceeae72b51d183bddc6a1e48c5b3dfa
Jacob Chesley <Jacob.Chesley@oit.edu>
2017-12-03 01:15:32 -0800

    Finished creating CreateAccountScreen.axml

--- EFRAndroidFrontEndTest/EFRFrontEndTest2/Resources/Resource.Designer.cs
+++ EFRAndroidFrontEndTest/EFRFrontEndTest2/Resources/Resource.Designer.cs
@@ -63,29 +63,77 @@ namespace EFRFrontEndTest2
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060005
-			public const int createAccountButton = 2131099653;
+			// aapt resource value: 0x7f060015
+			public const int CancelButton = 2131099669;
 			
-			// aapt resource value: 0x7f060001
-			public const int gridLayout1 = 2131099649;
-			
-			// aapt resource value: 0x7f060003
-			public const int loginButton = 2131099651;
-			
-			// aapt resource value: 0x7f06000c
-			public const int loginPageCharity = 2131099660;
-			
-			// aapt resource value: 0x7f06000a
-			public const int passwordBox = 2131099658;
-			
-			// aapt resource value: 0x7f06000b
-			public const int passwordCover = 2131099659;
-			
-			// aapt resource value: 0x7f060000
-			public const int relativeLayout1 = 2131099648;
+			// aapt resource value: 0x7f060013
+			public const int CreateButton = 2131099667;
 			
 			// aapt resource value: 0x7f060002
-			public const int textView2 = 2131099650;
+			public const int EmailBox = 2131099650;
+			
+			// aapt resource value: 0x7f060005
+			public const int EmailErrorBox = 2131099653;
+			
+			// aapt resource value: 0x7f060010
+			public const int FinalErrorBox = 2131099664;
+			
+			// aapt resource value: 0x7f06000b
+			public const int PasswordBox = 2131099659;
+			
+			// aapt resource value: 0x7f06000d
+			public const int PasswordErrorBox = 2131099661;
+			
+			// aapt resource value: 0x7f060009
+			public const int UsernameErrorBox = 2131099657;
+			
+			// aapt resource value: 0x7f06001a
+			public const int createAccountButton = 2131099674;
+			
+			// aapt resource value: 0x7f060007
+			public const int editText1 = 2131099655;
+			
+			// aapt resource value: 0x7f06000f
+			public const int editText2 = 2131099663;
+			
+			// aapt resource value: 0x7f060000
+			public const int gridLayout1 = 2131099648;
+			
+			// aapt resource value: 0x7f060019
+			public const int loginButton = 2131099673;
+			
+			// aapt resource value: 0x7f06001f
+			public const int loginPageCharity = 2131099679;
+			
+			// aapt resource value: 0x7f06001d
+			public const int passwordBox = 2131099677;
+			
+			// aapt resource value: 0x7f06001e
+			public const int passwordCover = 2131099678;
+			
+			// aapt resource value: 0x7f060001
+			public const int textView1 = 2131099649;
+			
+			// aapt resource value: 0x7f060011
+			public const int textView10 = 2131099665;
+			
+			// aapt resource value: 0x7f060012
+			public const int textView11 = 2131099666;
+			
+			// aapt resource value: 0x7f060014
+			public const int textView12 = 2131099668;
+			
+			// aapt resource value: 0x7f060016
+			public const int textView13 = 2131099670;
+			
+			// aapt resource value: 0x7f060017
+			public const int textView14 = 2131099671;
+			
+			// aapt resource value: 0x7f060018
+			public const int textView15 = 2131099672;
+			
+			// aapt resource value: 0x7f060003
+			public const int textView2 = 2131099651;
 			
 			// aapt resource value: 0x7f060004
 			public const int textView3 = 2131099652;
@@ -93,14 +141,23 @@ namespace EFRFrontEndTest2
 			// aapt resource value: 0x7f060006
 			public const int textView4 = 2131099654;
 			
-			// aapt resource value: 0x7f060007
-			public const int textView5 = 2131099655;
-			
 			// aapt resource value: 0x7f060008
-			public const int usernameBox = 2131099656;
+			public const int textView5 = 2131099656;
 			
-			// aapt resource value: 0x7f060009
-			public const int usernameCover = 2131099657;
+			// aapt resource value: 0x7f06000a
+			public const int textView6 = 2131099658;
+			
+			// aapt resource value: 0x7f06000c
+			public const int textView7 = 2131099660;
+			
+			// aapt resource value: 0x7f06000e
+			public const int textView8 = 2131099662;
+			
+			// aapt resource value: 0x7f06001b
+			public const int usernameBox = 2131099675;
+			
+			// aapt resource value: 0x7f06001c
+			public const int usernameCover = 2131099676;
 			
 			static Id()
 			{
