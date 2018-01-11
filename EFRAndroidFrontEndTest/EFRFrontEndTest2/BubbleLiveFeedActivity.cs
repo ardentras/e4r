cc5026e65f238565dda4119a6d0b16a66c0ee5c7
flowjo-shaunr <shaunr@flowjo.com>
2018-01-11 15:43:20 -0800

    Updated session renew to return user object

---
+++ EFRAndroidFrontEndTest/EFRFrontEndTest2/BubbleLiveFeedActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Views;
+using Android.Content;
+using Android.Widget;
+using System;
+using System.IO;
+using System.Json;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EFRFrontEndTest2
+{
+    [Activity(Label = "BubbleLiveFeedActivity")]
+    public class BubbleLiveFeedActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            RequestWindowFeature(WindowFeatures.NoTitle);
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.);
+        }
+
+        BubbleButton.Click += (sender, e) =>
+            {
+                //Closes the current view
+            }
+    
+    };
+}
+
+}
