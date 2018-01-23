3927e1adaab62b0ce56a263463cfa919790aecac
flowjo-shaunr <shaunr@flowjo.com>
2018-01-22 23:31:47 -0800

    added question create and drop table scripts

---
+++ database_scripts/drop_tables.sql
@@ -0,0 +1,13 @@
+DROP TABLE EFRQuest.QuestionHelp;
+DROP TABLE EFRQuest.Questions;
+DROP TABLE EFRQuest.QuestionsDB;
+DROP TABLE EFRQuest.Subjects;
+
+DROP TABLE EFRAcc.AchievementList;
+DROP TABLE EFRAcc.Achievements;
+DROP TABLE EFRAcc.Sessions;
+DROP TABLE EFRAcc.PasswordRecovery;
+DROP TABLE EFRAcc.CharityUsers;
+DROP TABLE EFRAcc.Charities;
+DROP TABLE EFRAcc.Users;
+DROP TABLE EFRAcc.PasswordSalts;
