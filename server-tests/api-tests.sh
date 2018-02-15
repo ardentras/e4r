380c576c208c40921bbaee6621b5252d01c7a368
flowjo-shaunr <shaunr@flowjo.com>
2018-02-14 20:16:58 -0800

    Added delete_user tests

--- server-tests/api-tests.sh
+++ server-tests/api-tests.sh
@@ -99,13 +99,47 @@ fi
 # Action: Send valid username and password
 # Expected Response: 200, session token
 ############################################
-curl -XPOST localhost:3002/api/login -sH 'Content-Type: application/json' -d '{"user":{"username":"abcde12345@gmail.com","password":"defaultpass"}}' >> response.json
+curl -XPOST localhost:3002/api/login -sH 'Content-Type: application/json' -d '{"user":{"username":"abcde12345@gmail.com","password":"defaultpass"}}' > response.json
 code=$(cat response.json | jq '.code')
 session_token=$(cat response.json | jq '.session_id')
 if [[ -z $session_token ]]; then
     echo "- Test log in request with valid account failed. Response did not contain session token"
-else if [[ ! $code -eq 200 ]]; then
+elif [[ ! $code -eq 200 ]]; then
     echo "- Test log in request with valid account failed. Expected server code 200, got ${code}"
 else
     echo "+ Test log in request with valid account passed"
 fi
+
+############################################
+# Endpoint: delete_user
+# Action: Send invalid session token
+# Expected Response: 401
+############################################
+code=$(curl -XDELETE localhost:3002/api/delete_user -sH 'Content-Type: application/json' -d '{"user":{"session":"imnotval-idxx-xxxx-xxxx-xxxxxxxxxxxx"}}' | jq '.code')
+if [[ ! $code -eq 401 ]]; then
+    echo "- Test delete user request with invalid session token failed. Expected server code 401, got ${code}"
+else
+    echo "+ Test delete user request with invalid session token passed"
+fi
+
+############################################
+# Endpoint: delete_user
+# Action: Send valid session token
+# Expected Response: 200, then 401, user invalid
+############################################
+code=$(curl -XDELETE localhost:3002/api/delete_user -sH 'Content-Type: application/json' -d '{"user":{"session":'$session_token'}}' | jq '.code')
+if [[ ! $code -eq 200 ]]; then
+    echo "- Test delete user request with invalid session token failed. Expected server code 200, got ${code}"
+else
+    code=$(curl -XPOST localhost:3002/api/login -sH 'Content-Type: application/json' -d '{"user":{"username":"abcde12345@gmail.com","password":"defaultpass"}}' | jq '.code')
+    if [[ ! $code -eq 401 ]]; then
+        echo "- Test delete user request with invalid session token failed. Expected server code 401, got ${code}"
+    else
+        echo "+ Test delete user request with invalid session token passed"
+    fi
+fi
+
+
+
+# Clean up mess
+rm response.json
