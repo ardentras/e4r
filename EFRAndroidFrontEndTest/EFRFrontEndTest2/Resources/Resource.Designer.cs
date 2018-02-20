1236ba6c3655dd0ebd0078957195568f0f084764
kelceystauffer <iluvgarebear@hotmail.com>
2018-02-19 23:00:33 -0800

    Added stars for difficulty

--- EFRAndroidFrontEndTest/EFRFrontEndTest2/Resources/Resource.Designer.cs
+++ EFRAndroidFrontEndTest/EFRFrontEndTest2/Resources/Resource.Designer.cs
@@ -102,28 +102,40 @@ namespace EFRFrontEndTest2
 			public const int MathIcon = 2130837522;
 			
 			// aapt resource value: 0x7f020013
-			public const int OptionsIcon = 2130837523;
+			public const int Normalstar = 2130837523;
 			
 			// aapt resource value: 0x7f020014
-			public const int PhysicsIcon = 2130837524;
+			public const int OptionsIcon = 2130837524;
 			
 			// aapt resource value: 0x7f020015
-			public const int redcross2 = 2130837525;
+			public const int PhysicsIcon = 2130837525;
 			
 			// aapt resource value: 0x7f020016
-			public const int RedCrossIcon = 2130837526;
+			public const int PurpleStar = 2130837526;
 			
 			// aapt resource value: 0x7f020017
-			public const int ShuffleIcon = 2130837527;
+			public const int redcross2 = 2130837527;
 			
 			// aapt resource value: 0x7f020018
-			public const int silverarrow = 2130837528;
+			public const int RedCrossIcon = 2130837528;
 			
 			// aapt resource value: 0x7f020019
-			public const int starIcon = 2130837529;
+			public const int RedStar = 2130837529;
 			
 			// aapt resource value: 0x7f02001a
-			public const int trophyIcon = 2130837530;
+			public const int ShuffleIcon = 2130837530;
+			
+			// aapt resource value: 0x7f02001b
+			public const int silverarrow = 2130837531;
+			
+			// aapt resource value: 0x7f02001c
+			public const int StarGreen = 2130837532;
+			
+			// aapt resource value: 0x7f02001d
+			public const int starIcon = 2130837533;
+			
+			// aapt resource value: 0x7f02001e
+			public const int trophyIcon = 2130837534;
 			
 			static Drawable()
 			{
