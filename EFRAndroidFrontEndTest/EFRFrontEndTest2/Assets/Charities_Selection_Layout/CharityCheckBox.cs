26cfedbd039e2d20af2e51a45e4c942080eb30cb
Kevin Xu <kevinjia.xu@gmail.com>
2018-03-06 06:38:32 -0800

    Improve UI and Layout creation, also added favorite functionality

---
+++ EFRAndroidFrontEndTest/EFRFrontEndTest2/Assets/Charities_Selection_Layout/CharityCheckBox.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace EFRFrontEndTest2.Assets.Charities_Selection_Layout
+{
+    public class CharityCheckBox
+    {
+        private CheckBox _button = null;
+        public CharityCheckBox(Context ctx)
+        {
+            CreateButton(ctx);
+        }
+        public CheckBox Button
+        {
+            get { return _button; }
+            set { _button = value; }
+        }
+        private void CreateButton(Context ctx)
+        {
+            CheckBox newBox = new CheckBox(ctx);
+            LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(50, 50)
+            {
+                LeftMargin = 20
+            };
+            newBox.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            newBox.LayoutParameters = param;
+            _button = newBox;
+        }
+    }
+}
