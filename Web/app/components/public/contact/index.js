0677c8206bf1574bb249e4ac439e30670d690339
kevin <kevinjia.xu@gmail.com>
2018-03-14 02:14:47 -0700

    removed debug href

--- Web/app/components/public/contact/index.js
+++ Web/app/components/public/contact/index.js
@@ -16,7 +16,7 @@ export default class Contact extends React.Component {
                     <h1 className={Styles.header}>CONTACT US</h1>
                     <h3 className={Styles.company}>Education for Revitalization</h3>
                     <p>phone: (123)456-7890</p>
-                    <p>email: info@efr.com</p>
+                    <p>email: e4rtesting@gmail.com</p>
                     <p className={Styles.location}>Location:</p>
                     <p>Klamath Falls, OR</p>
                     <form className={Styles.email} action="javascript:void(0);" onSubmit={this.sendEmail}>
@@ -27,7 +27,6 @@ export default class Contact extends React.Component {
                             <input type="email" name="email" placeholder="Where can we email you?"/>
                         </div>
                         <textarea name="content" cols="30" rows="10" placeholder="Spill the beans..."/>
-                        <a href="mailto:e4rtesting@gmail.com?Subject=Customer Support">dddd</a>
                         <input className={Styles.sendbtn} type="submit" value="Send"/>
                     </form>
                 </div>
