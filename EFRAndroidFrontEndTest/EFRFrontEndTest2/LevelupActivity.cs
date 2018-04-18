99ff096f7cd2cd21378120d9643b76b8f5aaf10e
ymmijx <ymmijxymmijx@gmail.com>
2018-04-17 22:16:17 -0700

    added code to change  background of dashboard and added popup code for level up popup.

--- EFRAndroidFrontEndTest/EFRFrontEndTest2/LevelupActivity.cs
+++ EFRAndroidFrontEndTest/EFRFrontEndTest2/LevelupActivity.cs
@@ -27,7 +27,7 @@ namespace EFRFrontEndTest2
             TextView lvText = FindViewById<TextView>(Resource.Id.lvLabel);
             UserObject obj = SingleUserObject.getObject();
             string text = Convert.ToString((int)(Math.Sqrt(obj.TotalQuestions / 10) + obj.TotalDonated / 50));
-            lvText.Text = "You are now level" + text; 
+            lvText.Text = "You are now level" + text + "!"; 
             var searchTimer = new Timer(600);
             searchTimer.Elapsed += delegate
             {
