0403613b6a78eb6831df4bab4f2bac78b1939365
tierein <tierein@gmail.com>
2018-05-03 15:50:11 -0700

    Updated home.cs fragment to have up to date functionality and deleted unused files.

--- EFRAndroidFrontEndTest/EFRFrontEndTest2/Fragments/Home.cs
+++ EFRAndroidFrontEndTest/EFRFrontEndTest2/Fragments/Home.cs
@@ -17,6 +17,7 @@ namespace EFRFrontEndTest2.Fragments
     public class Home : Android.Support.V4.App.Fragment
     {
         private UserObject uo = SingleUserObject.getObject();
+        private View view = null;
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -26,22 +27,80 @@ namespace EFRFrontEndTest2.Fragments
         public static Home NewInstance()
         {
             Home temp = new Home();
+
             return temp;
         }
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             //Use this to initialize all the click handlers and stuff
             //all UI related functionalities here
-            View view = inflater.Inflate(Resource.Layout.Home, container, false);
-            TextView uid_txt = view.FindViewById<TextView>(Resource.Id.uid_txt);
-            TextView level_txt = view.FindViewById<TextView>(Resource.Id.level_txt);
-            TextView solve_txt = view.FindViewById<TextView>(Resource.Id.solved_val);
-            TextView donate_txt = view.FindViewById<TextView>(Resource.Id.donate_val);
-            uid_txt.Text = uo.FirstName;
-            level_txt.Text = (uo.Difficulty + 1).ToString();
-            solve_txt.Text = uo.TotalQuestions.ToString();
-            donate_txt.Text = uo.TotalDonated.ToString();
+            view = inflater.Inflate(Resource.Layout.Home, container, false);
+            TextView user_fname = view.FindViewById<TextView>(Resource.Id.user_fname);
+            TextView user_level = view.FindViewById<TextView>(Resource.Id.user_level);
+            TextView total_solved = view.FindViewById<TextView>(Resource.Id.total_solved);
+            TextView total_donated = view.FindViewById<TextView>(Resource.Id.total_donated);
+            user_fname.Text = (uo.FirstName.Length > 0 ? uo.FirstName : uo.Username);
+            user_level.Text = uo.Level.ToString();
+            total_solved.Text = uo.TotalQuestions.ToString();
+            total_donated.Text = new StringBuilder().Append("$").Append(" ").Append(uo.TotalDonated).ToString();
+
             return view;
+        }
+
+        protected void setBackground()
+        {
+            if (AppBackground.background != null)
+            {
+                GridLayout background = view.FindViewById<GridLayout>(Resource.Id.settingsgrid);
+                background.Background = AppBackground.background;
+            }
         }
     }
 }
+
+
+// Code that hasnt been coppied over yet.
+/*
+using EFRFrontEndTest2.Assets.DynamicSize;
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            setBackground();
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            dynamicSizeRendering();
+        }
+        private void dynamicSizeRendering()
+        {
+            DynamicSize Device = new DynamicSize(Resources);
+            LinearLayout user_info_container = FindViewById<LinearLayout>(Resource.Id.linearLayout4);
+            LinearLayout control_btns = FindViewById<LinearLayout>(Resource.Id.control_btns);
+            TextView welcome = FindViewById<TextView>(Resource.Id.textView1);
+            TextView user_fname = FindViewById<TextView>(Resource.Id.user_fname);
+            TextView donated_text = FindViewById<TextView>(Resource.Id.donated_text);
+            TextView solved_text = FindViewById<TextView>(Resource.Id.solved_text);
+            TextView level_text = FindViewById<TextView>(Resource.Id.level_text);
+            TextView user_level = FindViewById<TextView>(Resource.Id.user_level);
+            TextView total_donated = FindViewById<TextView>(Resource.Id.total_donated);
+            TextView total_solved = FindViewById<TextView>(Resource.Id.total_solved);
+
+            control_btns.LayoutParameters = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MatchParent, LinearLayout.LayoutParams.WrapContent)
+            {
+                TopMargin = Device.DpHeight(0.1)
+            };
+            user_info_container.LayoutParameters = new LinearLayout.LayoutParams(Device.PxWidth(0.6), Device.PxHeight(0.3));
+            user_fname.SetPadding(0, Device.DpHeight(0.015), 0, Device.DpHeight(0.02));
+            user_fname.TextSize = Device.DpHeight(0.035);
+            user_level.TextSize = Device.DpHeight(0.03);
+            total_solved.TextSize = Device.DpHeight(0.03);
+            total_donated.TextSize = Device.DpHeight(0.03);
+            donated_text.TextSize = Device.DpHeight(0.020);
+            level_text.TextSize = Device.DpHeight(0.020);
+            solved_text.TextSize = Device.DpHeight(0.020);
+            welcome.TextSize = Device.DpHeight(0.020);
+        }
+}
+*/
