fad4afda3f96aba676f534d7debed88a498c4b11
Shaun Rasmusen <shaunrasmusen@gmail.com>
2018-05-29 13:00:09 -0700

    Added GET request to load test

--- app/loadtest/loadtest.sh
+++ app/loadtest/loadtest.sh
@@ -2,13 +2,14 @@
 
 echo "//========================================================================================//"
 echo ""
-echo "Running loadtest on /api/check_username endpoint."
+echo "Running load test on /api/check_username endpoint."
 echo ""
 echo "Sending username and email."
 echo ""
 echo "# of concurrent connections: 10"
-echo "# of requests per second:    500"
+echo "# of requests per second:    250"
 echo "Total runtime for this test: 20s"
+echo "Type of request:             POST"
 echo ""
 echo "//========================================================================================//"
 
@@ -16,13 +17,14 @@ loadtest -c 10 -t 20 --rps 500 -k -m POST -T 'application/json' --data '{"user":
 
 echo "//========================================================================================//"
 echo ""
-echo "Running loadtest on /api/login endpoint."
+echo "Running load test on /api/login endpoint."
 echo ""
 echo "Sending username and password."
 echo ""
 echo "# of concurrent connections: 10"
 echo "# of requests per second:    250"
 echo "Total runtime for this test: 20s"
+echo "Type of request:             POST"
 echo ""
 echo "//========================================================================================//"
 
@@ -30,13 +32,14 @@ loadtest -c 10 -t 20 --rps 250 -k -m POST -T 'application/json' --data '{"user":
 
 echo "//========================================================================================//"
 echo ""
-echo "Running loadtest on /api/update_uo endpoint."
+echo "Running load test on /api/update_uo endpoint."
 echo ""
 echo "Sending session_id token and user object."
 echo ""
 echo "# of concurrent connections: 10"
 echo "# of requests per second:    250"
 echo "Total runtime for this test: 20s"
+echo "Type of request:             PUT"
 echo ""
 echo "//========================================================================================//"
 
@@ -44,14 +47,28 @@ loadtest -c 10 -t 20 --rps 250 -k -m PUT -T 'application/json' --data '{"user":{
 
 echo "//========================================================================================//"
 echo ""
-echo "Running loadtest on /api/q/request_block endpoint."
+echo "Running load test on /api/q/request_block endpoint."
 echo ""
 echo "Sending session_id token, user object, and total donated."
 echo ""
 echo "# of concurrent connections: 10"
 echo "# of requests per second:    250"
 echo "Total runtime for this test: 20s"
+echo "Type of request:             PUT"
 echo ""
 echo "//========================================================================================//"
 
 loadtest -c 10 -t 20 --rps 250 -k -m PUT -T 'application/json' --data '{"user":{"session":"9c9e03a4-f951-46da-a1d8-5e0584877727","userobject":{"user_data":{"username":"shaunrasmusen","email":"shaunrasmusen@gmail.com","first_name":"","last_name":"","selected_charity":"","favorite_charities":[""]},"game_data":{"subject_name":"Science","subject_id":3,"difficulty":0,"totalQuestions":39,"totalDonated":0,"blocksRemaining":7,"completed_blocks":[60,5,164]}, "timestamp":"2018-01-24T02:06:58+00:00"}, "donated": "24"}}' http://34.216.143.255:3002/api/q/request_block
+
+echo "//========================================================================================//"
+echo ""
+echo "Running load test on /api/top_ten_q endpoint."
+echo ""
+echo "# of concurrent connections: 10"
+echo "# of requests per second:    500"
+echo "Total runtime for this test: 20s"
+echo "Type of request:             GET"
+echo ""
+echo "//========================================================================================//"
+
+loadtest -c 10 -t 20 --rps 500 -k http://34.216.143.255:3002/api/top_ten_q
